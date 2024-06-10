021b9d3e4ebe7d1351fe624c9303dc11bf00e4c1
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-06-10 16:57:03 -0300

    Ageitando bugs e adicionando novas funcionalidades e ageitando as listas

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
 
  // Criar evento de edição
-// criar uma funão que joga as informações em memôria local e depois exibe em tela; --- Em andamento......
-// Criar o sistema de concluido e pendentes -- Em andamento....
+
+ 
 // gerar o sistema de filtro
 // adicionar a api para traduzir o idioma 
 // Ageitar o ressponsivo
 
 
+var tamanho = localStorage.getItem('pending');
 
 
 
@@ -63,6 +64,10 @@ function atualizarData () {
     dayWeek.textContent = week[date.getUTCDay() - 1];
 }    
 
+
+atualizarProgresso()
+
+
 // Evita o comportamento dos formulários
 
 const formAddCard = document.querySelector(".formAddCard");
@@ -94,6 +99,7 @@ var criarCard = (inputValue) => {
 
     const divActions = document.createElement('div');
     divActions.classList.add('buttonsAction')
+    divActions.classList.add("escondido")
 
     const buttonRemove = document.createElement('button');
     buttonRemove.classList.add('buttonRemove');
@@ -124,6 +130,15 @@ var criarCard = (inputValue) => {
     form.appendChild(divActions);
 
     container.appendChild(form);
+    
+
+    // Ação de apresentar os botões de ação ao clicar
+
+    input.addEventListener('click', () =>{
+      divActions.classList.remove('escondido')
+      input.classList.remove('inputEscondido')
+    })
+
 
     // Joga o card criado para lista de pendentes no local storage
 
@@ -147,8 +162,9 @@ var criarCard = (inputValue) => {
     // Evento de conclução de card e adição na lista de concluidos ;
 
     buttonDone.addEventListener('click', () => {
-      
-      storegeLocal('Concluidos', inputAddCard)    
+
+      removerDaMemoria( 'pending' , inputAddCard )
+      storegeLocal( 'Concluidos', inputAddCard)    
       console.log(cardConcluidos)
       form.remove()
     
@@ -156,6 +172,73 @@ var criarCard = (inputValue) => {
 
     }
 };
+
+
+
+// Exibir em tela os elementos em memoria local pendentes
+
+var cardsPendentes = () => {
+  const buttonPending = document.querySelector('.peding');
+  
+  // função para remover todos os elementos em tela 
+  
+  removerElementos('listItems')
+  
+  // renderizar os elementos apenas dos pendentes 
+  
+  MostrarEmTela('pending');
+  
+}
+
+// Exibir em tela os elementos em memoria local concluidos
+
+var cardsConcluidos = () => {
+  const buttonPending = document.querySelector('.peding');
+  
+  // função para remover todos os elementos em tela 
+  
+  removerElementos('listItems')
+  
+  // renderizar os elementos apenas dos pendentes 
+  
+  MostrarEmTela('Concluidos');
+  
+}
+
+  
+  
+  
+  
+// função que renderiza elementos em local storage em tela  
+  
+var MostrarEmTela = ( chave ) => {
+  var value = JSON.parse(localStorage.getItem(`${chave}`));
+  
+
+  if (chave == 'pending'){
+    value.forEach( input => {
+      criarCardsLocaisPendentes(input);
+    })
+    
+
+
+  } else {
+    value.forEach( input => {
+      criarCardsLocaisConcluidos(input);
+    })
+  }
+
+}  
+  
+  
+  
+// Função que remove todos elementos de uma div  
+  
+var removerElementos = (container) => {
+  const containerArray = document.querySelector(`.${container}`);
+  containerArray.innerHTML = "";  
+}
+
 
 
 // Mandar o array com as tesks prontas para memoria local
@@ -172,10 +255,137 @@ var storegeLocal = (chave , valor) => {
   localStorage.setItem(chave, JSON.stringify(memoriaLocal));
 }
 
-// Exibir em tela os elementos em memoria local
+// Função para criar elementos dos cards salvos localmente
 
-const buttonPending = document.querySelector('.peding');
+var criarCardsLocaisPendentes = ( value ) => {
 
-buttonPending.addEventListener('click', () => {
+  const container = document.querySelector('.listItems')
 
-})
+  const form = document.createElement('form');
+  form.classList.add('itemCard');
+
+  form.addEventListener('submit' , (event) => {event.preventDefault()});
+
+  const input = document.createElement('input');
+  input.setAttribute('id','itemName')
+  input.placeholder = value;
+
+  const divActions = document.createElement('div');
+  divActions.classList.add('buttonsAction')
+  divActions.classList.add('escondido')
+
+  const buttonRemove = document.createElement('button');
+  buttonRemove.classList.add('buttonRemove');
+
+  const divRemove = document.createElement('div');
+  const iRemove = document.createElement('i');
+  iRemove.classList.add("fa-solid","fa-minus")
+
+
+  const buttonDone = document.createElement('button');
+  buttonDone.classList.add('buttonDone');
+
+
+  const divAdd = document.createElement('div');
+  const iDone = document.createElement('i');
+  iDone.classList.add("fa-solid" , "fa-check")
+
+  divRemove.appendChild(iRemove);
+  buttonRemove.appendChild(divRemove);
+
+  divAdd.appendChild(iDone);
+  buttonDone.appendChild(divAdd);
+
+  divActions.appendChild(buttonRemove);
+  divActions.appendChild(buttonDone);
+
+  form.appendChild(input);
+  form.appendChild(divActions);
+
+  container.appendChild(form);
+
+  
+  // Ação de apresentar os botões de ação ao clicar
+
+  input.addEventListener('click', () =>{
+    divActions.classList.remove('escondido')
+  })
+  
+
+  // Evento de remoção de card 
+
+  buttonRemove.addEventListener('click', () =>{ 
+
+    form.remove();
+
+  })
+
+  // Evento de conclução de card e adição na lista de concluidos ;
+
+  buttonDone.addEventListener('click', () => {
+    
+    removerDaMemoria( 'pending' , value )
+    storegeLocal( 'Concluidos', value )    
+    console.log(cardConcluidos)
+    form.remove()
+  
+  })
+
+  
+}
+
+
+var criarCardsLocaisConcluidos = (value) => {
+  const container = document.querySelector('.listItems')
+
+  const div = document.createElement('div');
+  div.classList.add('CardConcluido');
+
+  const span = document.createElement('span');
+  span.classList.add('spanText');
+  span.innerText = value;
+
+
+  div.appendChild(span);
+  container.appendChild(div)
+}
+
+// remove os elementos da memoria local
+
+var removerDaMemoria = (  chave , elementoParaRemover ) => {
+  var values  = JSON.parse(localStorage.getItem(`${chave}`));
+
+  if (  values ==  '' ){
+    values = [];
+  }
+
+  var RemoveValues  = values.filter( (  valor ) => { valor !== elementoParaRemover })
+
+  localStorage.setItem(`${chave}` , JSON.stringify(RemoveValues));
+
+}
+
+
+// ação que calcula a porcentagem da barra de conclução
+
+function atualizarProgresso () {
+
+  const progresso = document.querySelector('#progresso');
+  
+  
+  
+  var array =  JSON.parse(localStorage.getItem('pending'));
+  var array2 =  JSON.parse(localStorage.getItem('Concluidos'))
+
+
+  var total = array.length + array2.length;
+
+  var prog = ((array2 / 100) * total) / 100;
+  
+  teste = Math.round(prog);
+  
+  progresso.style.width = `${teste}%`
+  console.log(teste) 
+}
+
+
